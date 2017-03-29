a51c748f22daddf03595e91baf3b2d9989b90095
SuperCuke <pavelkrasner@gmail.com>
2017-03-29 17:06:22 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -14,74 +14,74 @@ const { syncHistoryWithStore } = require('react-router-redux');
 
 declare const __TEST__: boolean;
 var products = [{
-  id: 1,
-  name: "Item name 1",
-  price: 100
+    id: 1,
+    name: "Item name 1",
+    price: 100
 }, {
-  id: 2,
-  name: "Item name 2",
-  price: 100
+    id: 2,
+    name: "Item name 2",
+    price: 100
 }];
 
 const App = React.createClass({
-  render() {
-    return (
-      <div>
-        <OZ.HeaderBar>
-          <OZ.HeaderRightBar>
-            <OZ.HeaderMessages></OZ.HeaderMessages>
-            <OZ.HeaderTasks></OZ.HeaderTasks>
-            <OZ.HeaderNotifications></OZ.HeaderNotifications>
-            <OZ.HeaderUser></OZ.HeaderUser>
-            <OZ.HeaderSidebarControl></OZ.HeaderSidebarControl>
-          </OZ.HeaderRightBar>
-        </OZ.HeaderBar>
+    render() {
+        return (
+            <div>
+                <OZ.HeaderBar>
+                    <OZ.HeaderRightBar>
+                        <OZ.HeaderMessages></OZ.HeaderMessages>
+                        <OZ.HeaderTasks></OZ.HeaderTasks>
+                        <OZ.HeaderNotifications></OZ.HeaderNotifications>
+                        <OZ.HeaderUser></OZ.HeaderUser>
+                        <OZ.HeaderSidebarControl></OZ.HeaderSidebarControl>
+                    </OZ.HeaderRightBar>
+                </OZ.HeaderBar>
 
-        <OZ.MainSideBar></OZ.MainSideBar>
+                <OZ.MainSideBar></OZ.MainSideBar>
 
-        <div className="content-wrapper">
-          <section className="content-header">
-            <h1>
-              Page Header
+                <div className="content-wrapper">
+                    <section className="content-header">
+                        <h1>
+                            Page Header
           <small>Optional description</small>
-            </h1>
-            <ol className="breadcrumb">
-              <li><a href="#"><i className="fa fa-dashboard"></i> Level</a></li>
-              <li className="active">Here</li>
-            </ol>
-          </section>
-          {this.props.children}
-        </div>
+                        </h1>
+                        <ol className="breadcrumb">
+                            <li><a href="#"><i className="fa fa-dashboard"></i> Level</a></li>
+                            <li className="active">Here</li>
+                        </ol>
+                    </section>
+                    {this.props.children}
+                </div>
 
-        <footer className="main-footer">
-          <div className="pull-right hidden-xs">
-            Anything you want
+                <footer className="main-footer">
+                    <div className="pull-right hidden-xs">
+                        Anything you want
           </div>
-          <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
+                    <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
         </footer>
-        <OZ.ControlSideBar></OZ.ControlSideBar>
-        <div className="control-sidebar-bg"></div>
-      </div>
-    )
-  }
+                <OZ.ControlSideBar></OZ.ControlSideBar>
+                <div className="control-sidebar-bg"></div>
+            </div>
+        )
+    }
 })
 
 
 // It's a data format example.
 function priceFormatter(cell, row) {
-  return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
+    return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
 }
 
 if (!__TEST__) {
-  ReactDOM.render(
-    <Router history={hashHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Pages.FeatureFlags} />
-        <Route path="feature-flags" component={Pages.FeatureFlags} />
-        <Route path="data" component={Pages.DataPage} />
-        <Route path="monitoring" component={Pages.Monitoring} />
-      </Route>
-    </Router>
-    , document.getElementById('root')
-  );
+    ReactDOM.render(
+        <Router history={hashHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Pages.FeatureFlags} />
+                <Route path="feature-flags" component={Pages.FeatureFlags} />
+                <Route path="data" component={Pages.DataPage} />
+                <Route path="monitoring" component={Pages.Monitoring} />
+            </Route>
+        </Router>
+        , document.getElementById('root')
+    );
 }
--- src/pages/feature-flags.tsx
+++ src/pages/feature-flags.tsx
@@ -1,4 +1,4 @@
-﻿import * as React from 'react';
+import * as React from 'react';
 import * as RB from 'react-bootstrap';
 import * as OZ from '../components';
 
@@ -8,27 +8,27 @@ interface State {
 }
 
 export class FeatureFlags extends React.Component<Props, State> {
-    public render() {
-        return (
-            <section className="content">
-                <div className="box">
-                    <div className="box-header with-border">
-                        <h3 className="box-title">Bordered Table</h3>
-                    </div>
-                    <div className="box-body">
-                        <OZ.FeatureFlags></OZ.FeatureFlags>
-                    </div>
-                    <div className="box-footer clearfix">
-                        <ul className="pagination pagination-sm no-margin pull-right">
-                            <li><a href="#">&laquo;</a></li>
-                            <li><a href="#">1</a></li>
-                            <li><a href="#">2</a></li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#">&raquo;</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </section>
-        )
-    }
+  public render() {
+    return (
+      <section className="content">
+        <div className="box">
+          <div className="box-header with-border">
+            <h3 className="box-title">Bordered Table</h3>
+          </div>
+          <div className="box-body">
+            <OZ.FeatureFlags></OZ.FeatureFlags>
+          </div>
+          <div className="box-footer clearfix">
+            <ul className="pagination pagination-sm no-margin pull-right">
+              <li><a href="#">&laquo;</a></li>
+              <li><a href="#">1</a></li>
+              <li><a href="#">2</a></li>
+              <li><a href="#">3</a></li>
+              <li><a href="#">&raquo;</a></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    )
+  }
 }
